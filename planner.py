7b24380fbabd8bd803502c76b773d8f37ba11380
shreshthtuli <shreshthtuli@gmail.com>
2020-02-12 20:24:34 +0530

    Updated code

--- planner.py
+++ planner.py
@@ -1,3 +1,20 @@
 from husky_ur5 import *
 
-execute([["moveTo", "table"], ["changeWing", "up"], ["pick", "cube_red"]])
+res = execute([["moveTo", "cupboard"], \
+			   ["changeWing", "up"], \
+			   ["changeState", "cupboard", "open"], \
+			   ["moveTo", "cube_red"], \
+			   ["pick", "cube_red"], \
+			   ["moveTo", "cupboard"], \
+			   ["dropTo", "cupboard"], \
+			   ["moveTo", "cube_green"], \
+			   ["pick", "cube_green"], \
+			   ["moveTo", "cupboard"], \
+			   ["dropTo", "cupboard"], \
+			   ["moveTo", "cube_gray"], \
+			   ["pick", "cube_gray"], \
+			   ["moveTo", "cupboard"], \
+			   ["dropTo", "cupboard"], \
+			   ])
+
+print(res)
