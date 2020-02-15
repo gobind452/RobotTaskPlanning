f3b62174c4f363a1fbe7fd326c17c4e6f537196f
shreshthtuli <shreshthtuli@gmail.com>
2020-02-15 18:50:43 +0530

    Updated sample codes

---
+++ environment.py
@@ -0,0 +1,27 @@
+from husky_ur5 import *
+from copy import deepcopy
+
+state = getCurrentState()
+print(state)
+
+objects = ['apple', 'orange', 'banana', 'table', 'table2', 'box', 'fridge', 'tray', 'tray2']
+enclosures = ['fridge']
+actions = ['moveTo', 'pick', 'drop', 'changeState', 'pushTo']
+
+# A checker for action feasibility
+def checkAction(state, action):
+	# Check if action is possible for state
+	return True
+
+# An approximate environment model
+def changeState(state1, action):
+	state = deepcopy(state1)
+	# Change state based on action
+	return state
+
+# An approximate goal checker
+def checkGoal(state):
+	return ('apple', 'fridge') in state['inside'] and \
+		   ('orange', 'fridge') in state['inside'] and \
+		   ('banana', 'fridge') in state['inside'] and \
+		   state['fridge'] == 'Close'
--- planner.py
+++ planner.py
@@ -1,20 +1,27 @@
-from husky_ur5 import *
+from environment import *
 
-res = execute([["moveTo", "cupboard"], \
+def getPlan():
+	return [["moveTo", "fridge"], \
 			   ["changeWing", "up"], \
-			   ["changeState", "cupboard", "open"], \
-			   ["moveTo", "cube_red"], \
-			   ["pick", "cube_red"], \
-			   ["moveTo", "cupboard"], \
-			   ["dropTo", "cupboard"], \
-			   ["moveTo", "cube_green"], \
-			   ["pick", "cube_green"], \
-			   ["moveTo", "cupboard"], \
-			   ["dropTo", "cupboard"], \
-			   ["moveTo", "cube_gray"], \
-			   ["pick", "cube_gray"], \
-			   ["moveTo", "cupboard"], \
-			   ["dropTo", "cupboard"], \
-			   ])
+			   ["changeState", "fridge", "open"], \
+			   ["moveTo", "apple"], \
+			   ["pick", "apple"], \
+			   ["moveTo", "fridge"], \
+			   ["dropTo", "fridge"], \
+			   ["moveTo", "orange"], \
+			   ["pick", "orange"], \
+			   ["moveTo", "fridge"], \
+			   ["dropTo", "fridge"], \
+			   ["moveTo", "banana"], \
+			   ["pick", "banana"], \
+			   ["moveTo", "fridge"], \
+			   ["dropTo", "fridge"], \
+			   ["changeState", "fridge", "close"], \
+			   ]
+
+# Execute function takes in a plan as input and returns if goal constraints 
+# are valid and the final state after plan execution
+res, state = execute(getPlan())
 
 print(res)
+print(state)
