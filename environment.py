5e5083d17253320ce48fe53710928fec76f05669
gobind452m <gobind452@gmail.com>
2020-04-01 10:58:59 +0530

    Forward

--- environment.py
+++ environment.py
@@ -15,11 +15,14 @@ objects = set(['apple', 'orange', 'banana', 'table', 'table2', 'box', 'fridge', 
 enclosures = set(['fridge', 'cupboard']) # Can be open or closed
 canContain = set(['fridge','cupboard','box']) # Can contain objects (In predicate)
 canSupport = set(['table','table2','tray','tray2']) # Can support objects (On predicate)
-actionTemplates = ['moveTo', 'pick', 'drop', 'changeState', 'pushTo'] 
-canPlaced = set(['apple','orange','banana','tray','tray2']) # Can be placed in or on
+actionTemplates = ['moveTo', 'pick', 'drop', 'changeState', 'pushTo','transfer'] 
+canPlaced = set(['apple','orange','banana','tray','tray2','box']) # Can be placed in or on
 cantPick = set(['fridge','cupboard','table','table2']) # Cant pick these objects
+constraints = set([('box','tray'),('box','tray2')])
+
 actionGroundings = set()
 goalBasedActionGroundings = set()
+transferActionGroundings = set()
 
 class Environment(object):
 	def __init__(self,args):
@@ -30,7 +33,6 @@ class Environment(object):
 		self.currState = {'grabbed':state['grabbed'],'fridge':state['fridge'].lower(),'cupboard':state['cupboard'].lower(),'close':set(state['close'])}
 		self.currState['inside'] = dict([(e,set()) for e in canContain])
 		self.currState['on'] = dict([(e,set()) for e in canSupport])
-		self.currState['near'] = dict([(e,set()) for e in objects])
 		for rel in state['inside']:
 			self.currState['inside'][rel[1]].add(rel[0])
 		for rel in state['on']:
@@ -51,11 +53,7 @@ class Environment(object):
 				self.goal.append((obj,target,"on"))
 
 	def checkAction(self,state, action):
-		if action[0] == "moveTo":
-			if action[1] in state['close']:
-				return False # If object is already close, cant take this action
-		
-		elif action[0] == "pick":
+		if action[0] == "pick":
 			if action[1] not in state['close']:
 				return False # First get close to the object
 			if action[1] in cantPick:
@@ -75,6 +73,8 @@ class Environment(object):
 			if action[1] in enclosures:
 				if state[action[1]].lower() == "close":
 					return False # First open enclosure
+			if (state['grabbed'],action[1]) in constraints:
+				return False
 		
 		elif action[0] == "changeState":
 			if state['grabbed']!= "":
@@ -95,8 +95,26 @@ class Environment(object):
 				return False # Destination is start
 			if action[1] in cantPick: # Cant pick this object
 				return False
-			if action[2] in state['near'][action[1]]: # Object is already close to destination
+		
+		elif action[0] == "transfer":
+			if action[2] not in canContain and action[2] not in canSupport:
 				return False
+			if (action[1],action[2]) in constraints:
+				return False
+			if action[1] not in canPlaced:
+				return False
+			if state["grabbed"] != "":
+				return False
+			if action[2] in enclosures and state[action[2]].lower() == "close":
+				return False
+			if action[1] == action[2]:
+				return False
+			if action[2] in canSupport:
+				if action[1] in state['on'][action[2]]:
+					return False
+			elif action[2] in canContain:
+				if action[1] in state['inside'][action[2]]:
+					return False
 
 		return True
 
@@ -161,6 +179,12 @@ class Environment(object):
 			state = self.changeState(state,["moveTo",action[2]]) # Move to destination
 			state['grabbed'] = "" # Leave object here
 
+		elif action[0] == "transfer":
+			state = self.changeState(state,["moveTo",action[1]])
+			state = self.changeState(state,["pick",action[1]])
+			state = self.changeState(state,["moveTo",action[2]])
+			state = self.changeState(state,["drop",action[2]])
+
 		return state
 
 	def buildActionGroundings(self):
@@ -182,41 +206,46 @@ class Environment(object):
 					continue
 				actionGroundings.add(("pushTo",obj,obj1))
 		global goalBasedActionGroundings
-		relevantObjects = set()
+		self.relevantObjects = set()
 		for predicate in self.goal:
 			if predicate[-1] == "is":
-				relevantObjects.add(predicate[0].lower())
+				self.relevantObjects.add(predicate[0].lower())
 			else:
-				relevantObjects.add(predicate[0])
-				relevantObjects.add(predicate[1])
+				self.relevantObjects.add(predicate[0])
+				self.relevantObjects.add(predicate[1])
 		firstOrderRelevantObjects = set()
 		for obj in canContain:
 			for obj1 in self.currState['inside'][obj]:
-				if obj1 in relevantObjects:
+				if obj1 in self.relevantObjects:
 					firstOrderRelevantObjects.add(obj)
 		for obj in canSupport:
 			for obj1 in self.currState['on'][obj]:
-				if obj1 in relevantObjects:
+				if obj1 in self.relevantObjects:
 					firstOrderRelevantObjects.add(obj)
-		relevantObjects = relevantObjects.union(firstOrderRelevantObjects)
-		del firstOrderRelevantObjects
-		for obj in relevantObjects:
+		self.relevantObjects = self.relevantObjects.union(firstOrderRelevantObjects)
+		for obj in self.relevantObjects:
 			goalBasedActionGroundings.add(("moveTo",obj))
-		for obj in relevantObjects-cantPick:
+		for obj in self.relevantObjects-cantPick:
 			goalBasedActionGroundings.add(("pick",obj))
-		for obj in relevantObjects.intersection(canSupport):
+		for obj in self.relevantObjects.intersection(canSupport):
 			goalBasedActionGroundings.add(("drop",obj))
-		for obj in relevantObjects.intersection(canContain):
+		for obj in self.relevantObjects.intersection(canContain):
 			goalBasedActionGroundings.add(("drop",obj))
-		for obj in relevantObjects.intersection(enclosures):
+		for obj in self.relevantObjects.intersection(enclosures):
 			goalBasedActionGroundings.add(("changeState",obj,"open"))
 			goalBasedActionGroundings.add(("changeState",obj,"close"))
-		for obj in relevantObjects-cantPick:
-			for obj1 in relevantObjects:
+		for obj in self.relevantObjects-cantPick:
+			for obj1 in self.relevantObjects:
 				if obj == obj1 :
 					continue
 				goalBasedActionGroundings.add(("pushTo",obj,obj1))
-		
+		for obj in self.relevantObjects-firstOrderRelevantObjects:
+			for obj1 in self.relevantObjects-firstOrderRelevantObjects:
+				if obj == obj1:
+					continue
+				transferActionGroundings.add(("transfer",obj,obj1))
+		del firstOrderRelevantObjects
+
 
 	def getApplicableActions(self,state,eliminate=False):
 		applicableActions = list()
@@ -228,14 +257,16 @@ class Environment(object):
 			for action in goalBasedActionGroundings:
 				if self.checkAction(state,list(action)) == True:
 					applicableActions.append(action)
-		elif eliminate == "aggressive": # Remove the pushTo action
-			for action in goalBasedActionGroundings:
-				if self.checkAction(state,list(action)) == True and action[0] != "pushTo":
+		elif eliminate == "aggressive": # Only the transfer action
+			for action in transferActionGroundings:
+				if self.checkAction(state,list(action)) == True:
 					applicableActions.append(action)
 		return applicableActions
 		
-	def checkGoal(self,state):
+	def checkGoal(self,state,ignore=False):
 		for predicate in self.goal:
+			if predicate[-1] == "is" and ignore == True:
+				continue
 			check = self.checkPredicate(state,predicate)
 			if check == False:
 				return False
@@ -248,3 +279,12 @@ class Environment(object):
 			return predicate[0] in state["inside"][predicate[1]]
 		else:
 			return predicate[0] in state["on"][predicate[1]]
+
+	def canBePlaced(self,obj1,obj2):
+		if obj1 not in canPlaced:
+			return False
+		if obj2 not in canSupport and obj2 not in canContain:
+			return False
+		if (obj1,obj2) in constraints:
+			return False
+		return True
--- planner.py
+++ planner.py
@@ -1,6 +1,7 @@
 from environment import Environment
 from src.parser import *
 from husky_ur5 import execute
+import gc
 
 args = initParser()
 
@@ -9,23 +10,76 @@ depth = 10
 class Planner(object):
 	def __init__(self,env):
 		self.env = env
+		self.buildHeuristicTree()
 
-	def sortByHeuristic(self,state,actions):
-		scores = []
+	def buildHeuristicTree(self):
+		self.parents = dict() # Mapping from objects to parents
+		for predicate in self.env.goal:
+			if predicate[-1] == "is":
+				continue
+			if predicate[0] not in self.parents.keys():
+				self.parents[predicate[0]] = set()
+			if predicate[1] not in self.parents.keys():
+				self.parents[predicate[1]] = set()
+			self.parents[predicate[0]].add(predicate[1])
+		stop = False
+		for i in range(len(self.parents.keys())):
+			stop = True
+			for obj in self.parents.keys():
+				if len(self.parents[obj]) <= 1:
+					continue
+				stop = False
+				parent = ""
+				if "tray" in self.parents[obj]: # Tray, tray2 and box are the only objects that can be intermediate
+					parent = "tray"
+				elif "tray2" in self.parents[obj]:
+					parent = "tray2"
+				elif "box" in self.parents[obj]:
+					parent = "box"
+				if parent != "":
+					self.parents[parent] = self.parents[parent].union(self.parents[obj])
+					self.parents[parent].discard(parent)
+					self.parents[obj] = set([parent])
+			if stop == True:
+				break
+		for obj in self.parents.keys():
+			if len(self.parents[obj]) == 0:
+				self.parents[obj] = ""
+			else:
+				self.parents[obj] = list(self.parents[obj])[0]
+		self.depths = dict([(e,0) for e in self.parents.keys()])
+		stack = []
+		visited = set()
+		for obj in self.parents.keys():
+			if obj in visited:
+				continue
+			visited.add(obj)
+			stack.append(obj)
+			while self.parents[stack[-1]] != "":
+				if self.parents[stack[-1]] in visited:
+					stack.append(self.parents[stack[-1]])
+					break
+				visited.add(self.parents[stack[-1]])
+				stack.append(self.parents[stack[-1]])
+			for i,obj1 in enumerate(reversed(stack)):
+				self.depths[obj1] = i + self.depths[stack[-1]]
+			stack = []
+
+	def sortByHeuristic(self,actions):
+		score = dict()
 		for action in actions:
-			state = self.env.changeState(state,action)
-			scores.append(sum([self.scoreStateForPredicate(state,predicate) for predicate in self.env.goal]))
-		actions = zip(scores,actions)
-		actions = [x for _,x in sorted(actions)]
+			score[action] = [0,self.depths[action[1]]] # Emphasize leaf nodes and parent-child
+			temp = action[1]
+			while temp !="":
+				if temp == action[2]:
+					score[action][0] = self.depths[action[2]]-self.depths[action[1]]
+					break
+				temp = self.parents[temp]
+			if temp == "":
+				score[action][0] = -10
+		actions = list(actions)
+		actions.sort(key=lambda x:tuple(score[x]))
 		return actions
-
-	def scoreStateForPredicate(self,state,predicate):
-		if predicate[-1] == "is":
-			return int(state[predicate[0]].lower() == predicate[1].lower())*10
-		elif predicate[-1] == "in":
-			return int(predicate[0] in state["inside"][predicate[1]])*10
-		elif predicate[-1] == "on":
-			return int(predicate[0] in state["on"][predicate[1]])*10
 
 	def searchForPlan(self):
 		global depth
@@ -33,32 +87,54 @@ class Planner(object):
 		stack = [] # DFS Stack
 		actionsTaken = [] # Action stack
 		actionsSorting = [] # Actions Sort
+		if env.checkGoal(self.env.currState,ignore=False) == True:
+			return []
+		if 'fridge' in self.env.relevantObjects:
+			actionsTaken.extend([["moveTo","fridge"],["changeState","fridge","open"]])
+		if 'cupboard' in self.env.relevantObjects:
+			actionsTaken.extend([["moveTo","cupboard"],["changeState","cupboard","open"]])
+		for action in actionsTaken:
+			state = self.env.changeState(state,action)
 		stack.append(state)
+		initLength = len(actionsTaken)
+		goalDone = False
 		while len(stack)!=0:
-			#print(actionsTaken)
-			if env.checkGoal(stack[-1]) == True:
-				return actionsTaken # Return plan
+			if env.checkGoal(stack[-1],ignore=True) == True:
+				goalDone = True
+				break
 			if len(stack) == depth: # Reached search depth
 				del stack[-1]
 				del actionsTaken[-1]
 			if len(stack) > len(actionsSorting): # Add actions sort
-				applicableActions = self.env.getApplicableActions(stack[-1],eliminate=True)
-				actionsSorting.append(applicableActions)
+				applicableActions = self.env.getApplicableActions(stack[-1],eliminate="aggressive")
+				actionsSorting.append(self.sortByHeuristic(applicableActions))
 			if len(actionsSorting[-1]) == 0: # All actions taken (this state exhausted)
 				del stack[-1]
 				del actionsSorting[-1]
-				if len(actionsTaken)>0:
+				if len(actionsTaken)>initLength:
 					del actionsTaken[-1]
 			else:
 				actionsTaken.append(actionsSorting[-1][-1])
 				del actionsSorting[-1][-1]
 				stack.append(self.env.changeState(stack[-1],list(actionsTaken[-1])))
-		return None
+		plan = []
+		if goalDone == False:
+			return []
+		for action in actionsTaken:
+			if action[0] == "transfer":
+				plan.extend([["moveTo",action[1]],["pick",action[1]],["moveTo",action[2]],["drop",action[2]]])
+			else:
+				plan.append(action)
+		for predicate in self.env.goal:
+			if predicate[-1] == "is":
+				if predicate[1] == "close":
+					plan.extend([["moveTo",predicate[0]],["changeState",predicate[0],"close"]])
+		return plan
 
 #@deadline(120)
 def getPlan(planner):
-	return [["moveTo","apple"],["pick","apple"],["moveTo","box"],["drop","box"], ["pushTo","box","fridge"], ["changeState","fridge","open"], ["pick","box"], ["drop","fridge"]]
-	return planner.searchForPlan(env)
+	return []
+	#return planner.searchForPlan()
 	
 if args.symbolic == "yes":
 	env = Environment(args)
@@ -66,6 +142,9 @@ if args.symbolic == "yes":
 	plan = getPlan(planner)
 	print(plan)
 else:
-	res, state = execute(getPlan(None))
-	print(res)
+	env = Environment(args)
+	planner = Planner(env)
+	plan = getPlan(planner)
+	res, state = execute(plan)
+	print(res,"Goal satisfied")
 	print(state)
